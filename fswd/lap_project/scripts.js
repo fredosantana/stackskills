6cd8eef918647b0fd7e8b7a4818dff698ffa0e34
fredosantana <santana.wil@gmail.com>
2017-08-20 13:05:53 -0400

    Project subdirectory created; HTML template director and mynote file moved

--- fswd/lap_project/scripts.js
+++ fswd/lap_project/scripts.js
@@ -1,8 +1,17 @@
-function myFunction() {
+/*function myFunction() {
   var inpObj = document.getElementById("zip");
     if (inpObj.checkValidity() == false) {
-      document.getElementById("demo").innerHTML = inpObj.validationMessage;
+      document.getElementById("zmessage").innerHTML = inpObj.validationMessage;
     } else {
-      document.getElementById("demo").innerHTML = "Input OK";
+      document.getElementById("zmessage").innerHTML = "Zip OK";
+    }
+}*/
+
+function thisFunction() {
+  var inpObj = document.getElementById("ss");
+    if (inpObj.checkValidity() == false) {
+      document.getElementById("ssmessage").innerHTML = inpObj.validationMessage;
+    } else {
+      document.getElementById("ssmessage").innerHTML = "SS OK";
     }
 }
