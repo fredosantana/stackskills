04f84196772cd6f3f95b1b0b91c8ddafc0d2ff9b
fredosantana <santana.wil@gmail.com>
2017-08-31 06:47:49 -0400

    If/else statement added to exercise 2

--- fswd/05_javascript/02_loops-conditionals-switches/script.js
+++ fswd/05_javascript/02_loops-conditionals-switches/script.js
@@ -6,10 +6,39 @@ var i = 0;
 while(i < 10) {
   console.log(i);
   i++;
-};*/
+};
 
 var people = ["Jim", "James", "Paul", "Tyrone"];
 
 for (var i = 0; i < people.length; i++) {
   console.log(people[i]);
 }
+
+var acorn_dr = [
+  {
+    name: 'Wil',
+    age: 37
+  },
+  {
+    name: 'Wayne',
+    age: 53
+  },
+  {
+    name: 'Lucas',
+    age: 'FOREVER!!!'
+  }
+];
+
+var output = ' ';
+
+for (var i = 0; i < acorn_dr.length; i++) {
+  document.getElementById('booger_lips').innerHTML += '<li>'+acorn_dr[i].name+' - '+acorn_dr[i].age+'</li>';
+}*/
+
+var x = 25;
+var y = 15;
+if (x < y) {
+  console.log(x + ' is less than ' + y);
+} else {
+  console.log(x + ' is more than ' + y);
+};
