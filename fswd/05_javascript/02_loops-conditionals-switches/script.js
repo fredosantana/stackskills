d90ee390c4e8d02d8e616c8db04b6a439207000c
fredosantana <santana.wil@gmail.com>
2017-08-31 06:28:23 -0400

    Loops, conditionals, and switches exercise started; given their own subdirectory; external js used instead of inline

---
+++ fswd/05_javascript/02_loops-conditionals-switches/script.js
@@ -0,0 +1,15 @@
+/*for(i = 0; i <= 10; i++) {
+  console.log(i);
+};
+
+var i = 0;
+while(i < 10) {
+  console.log(i);
+  i++;
+};*/
+
+var people = ["Jim", "James", "Paul", "Tyrone"];
+
+for (var i = 0; i < people.length; i++) {
+  console.log(people[i]);
+}
