bf93bfb2f0e32f7897fe0b4e6d1a445b0ff8c039
fredosantana <santana.wil@gmail.com>
2017-08-31 16:40:10 -0400

    Finished JS loops, conditionals, and switches exercise

--- fswd/05_javascript/02_loops-conditionals-switches/script.js
+++ fswd/05_javascript/02_loops-conditionals-switches/script.js
@@ -1,4 +1,6 @@
-/*for(i = 0; i <= 10; i++) {
+/*
+
+for(i = 0; i <= 10; i++) {
   console.log(i);
 };
 
@@ -33,12 +35,34 @@ var output = ' ';
 
 for (var i = 0; i < acorn_dr.length; i++) {
   document.getElementById('booger_lips').innerHTML += '<li>'+acorn_dr[i].name+' - '+acorn_dr[i].age+'</li>';
-}*/
+}
 
-var x = 25;
-var y = 15;
-if (x < y) {
-  console.log(x + ' is less than ' + y);
-} else {
+var x = 20;
+var y = 25;
+var z = 10;
+if (x < y && z == 10) {
+  console.log(x + ' is less than ' + y + ' and z is 10.');
+}
+else if(x > y) {
   console.log(x + ' is more than ' + y);
+}
+else {
+  console.log(x + ' is equal to ' + y);
 };
+
+*/
+
+var name = 'Lucas';
+switch(name) {
+  case 'Wil':
+    alert('My name is Wil.');
+    break;
+  case 'Wayne':
+    alert('My name is Wayne.');
+    break;
+  case 'Lucas':
+    alert('BARK!');
+    break;
+  default:
+    alert('That person and/or dog doesn\'t live in Acorn Drive.')
+}
