4947d9c6ce56d1d04c2eab77f54bf432d894bbdb
fredosantana <santana.wil@gmail.com>
2017-08-31 17:08:30 -0400

    Function and events subdirectory created; variables, arrays, and objects lesson moved to its own subdirectory

---
+++ fswd/05_javascript/03_functions-and-events/script.js
@@ -0,0 +1,11 @@
+/*
+function sayWord() {
+  alert('Word!');
+}
+
+function sayWord(anything = 'What\'s', everything = 'Up!'){
+  alert(anything + ' ' + everything);
+}
+
+sayWord('Word','Son!');
+*/
