25b88077e1b33161138efbc0ed69e85db74841ce
fredosantana <santana.wil@gmail.com>
2017-08-31 21:14:50 -0400

    todayDate function added

--- fswd/05_javascript/03_functions-and-events/script.js
+++ fswd/05_javascript/03_functions-and-events/script.js
@@ -2,10 +2,24 @@
 function sayWord() {
   alert('Word!');
 }
-*/
+
 
 function sayWord(anything = 'What\'s', everything = 'Up', something = "Wil!" ){
   alert(anything + '-' + everything + '-' + something);
 }
 
 sayWord("BOOM", "SHAKA", "LAKA!!!!");
+
+var sum = getSum(4, 5);
+
+function getSum(pizza, cheeseburger){
+  return pizza + cheeseburger;
+}
+
+console.log('The answer is ' + getSum(78947923947, 7987894823427));
+*/
+
+function todaysDate() {
+  var date = new Date();
+  document.getElementById('showDate').innerHTML = date;
+}
