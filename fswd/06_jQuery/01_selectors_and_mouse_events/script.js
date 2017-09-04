b9c821cd9cd074abf8607d639a91fe369c8ae1f6
fredosantana <santana.wil@gmail.com>
2017-09-04 16:18:11 -0400

    Completed first jQuery exercise

--- fswd/06_jQuery/01_selectors_and_mouse_events/script.js
+++ fswd/06_jQuery/01_selectors_and_mouse_events/script.js
@@ -9,10 +9,36 @@ $(document).ready(function(){
     $('.p1').toggle();
     //$('.p2').show('Test');
   });
-  */
+
   $('#touchIt').dblclick(function(){
     //alert('BAM!!!');
     $('.p1').toggle();
     $('.p2').toggle();
+  });
+  $('#rectangle').hover(function(){
+    $(this).text("HOLLA!!!");
+  }, function() {
+      $(this).text("COME BACK!!");
+    }
+  );
+
+  $('#rectangle').mouseenter(function() {
+    $(this).text('VANDALAYY!!!');
+    $(this).mouseout(function() {
+      $(this).text('SAY VANDALAY!!!');
+    });
+  });
+
+  $('#rectangle').mousedown(function() {
+    $(this).text('NOW TAKE YOUR SMELLY FINGER OFF THE MOUSE!!!!!');
+    $(this).mouseup(function() {
+      $(this).text('AND CLICK IT DOWN AGAIN!!!');
+    });
+  });
+  */
+  $('#rectangle2').mousemove(function(event){
+    var proteinShake = "Mouse position - ";
+    proteinShake += event.pageX + " " + event.pageY;
+    $('#alarmClock').html('<div>' + proteinShake + '</div>');
   });
 });
