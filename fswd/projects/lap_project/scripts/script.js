cbb729257925a01af2c8103311bc04f34c5cdb46
fredosantana <santana.wil@gmail.com>
2017-09-07 17:04:43 -0400

    Validations added to ss variable; zip code and ss field pushed down for form consistency

--- fswd/projects/lap_project/scripts/script.js
+++ fswd/projects/lap_project/scripts/script.js
@@ -1,34 +1,3 @@
-/* Function validation for zip code
-   Intended to accept only five digits
-   Will accept all number lower than 99999
-   Needs to be fixed as of 8/20/2017
-*/
-
-function myFunction() {
-  var inpObj = document.getElementById('zip');
-    if (inpObj.checkValidity() == false) {
-      document.getElementById('demo').innerHTML = inpObj.validationMessage;
-    } else {
-      document.getElementById('demo').innerHTML = "Zip OK";
-    }
-}
-
-/* Function validation for ss number
-   Intended to accept only four digits
-   Works on its own but can't work together with myFunction
-   Needs to be fixed as of 8/20/2017
-
-function thisFunction() {
-  var inpObj = document.getElementById("ss");
-    if (inpObj.checkValidity() == false) {
-      document.getElementById("ssmessage").innerHTML = inpObj.validationMessage;
-    } else {
-      document.getElementById("ssmessage").innerHTML = "SS OK";
-    }
-}
-
-*/
-
 function clientForm(e) {
   e.preventDefault();
 
@@ -52,17 +21,21 @@ function clientForm(e) {
         message.className = "alert alert-danger";
         message.innerHTML = 'First and last name must be at least 3 characters';
       } else {
-        if(zip.length < 5 || zip.length > 5) {
-          message.className = "alert alert-danger";
-          message.innerHTML = 'Zip code must be five numbers';
-        }       else {
-                // SUCCESS
+          if(zip.length < 5 || zip.length > 5) {
+            message.className = "alert alert-danger";
+            message.innerHTML = 'Zip code must be five numbers';
+          } else {
+              if(ss.length < 4 || ss.length > 4) {
+                message.className = "alert alert-danger";
+                message.innerHTML = 'SS number must be four digits';
+              } else {
+            // SUCCESS
                 message.className = "alert alert-success";
                 message.innerHTML = ('Client name: ' + firstName + ' ' + lastName + '\nAddress: ' + address + ' ' + city + ', ' + state + ' ' + zip + '\nPhone number: ' + phone + '\nID number: ' + ss + '\nProgram name: ' + program);
-              }
+             }
             }
-
+          }
+        }
       }
-}
 
 document.getElementById('clientInfo').addEventListener('submit', clientForm, false);
