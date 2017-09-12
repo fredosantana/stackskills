ee0ed3c2a6dde9b340f7b1432491c7909193b3bf
fredosantana <santana.wil@gmail.com>
2017-09-12 17:12:11 -0400

    Extra validations added to signin page

--- fswd/projects/lap_project/scripts/signIn.js
+++ fswd/projects/lap_project/scripts/signIn.js
@@ -5,18 +5,23 @@ function signIn(e){
   var psw2 = document.getElementById('psw2').value;
   var message = document.getElementById('message');
 
-  if(uname != 'wil.santana@mbhp.org') {
+  if (uname == '' || psw == '' || psw2 == '') {
     message.className = "alert alert-danger"
-    message.innerHTML = 'Username incorrect';
+    message.innerHTML = 'Stop giving me blanks. I need more!!';
   } else {
-      if(psw != 'Kw071517!' && psw2 != 'Kw071517!'){
-        message.className = "alert alert-danger";
-        message.innerHTML = 'Passwords do not match';
-        } else {
+      if(uname != 'wil.santana@mbhp.org') {
+        message.className = "alert alert-danger"
+        message.innerHTML = 'I don\'t even know you. STRANGER!!!';
+      } else {
+          if(psw != 'Kw071517!' && psw2 != 'Kw071517!'){
+            message.className = "alert alert-danger";
+            message.innerHTML = 'It\'s gotta match for me to turn on';
+          } else {
               //SUCCESS
               window.location = "forms/request.html";
+            }
+          }
         }
       }
-    }
 
 document.getElementById('regForm').addEventListener('submit', signIn, false);
