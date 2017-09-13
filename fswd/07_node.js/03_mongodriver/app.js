333d0d0a30222e9a84e27630a5337c1308dc4973
fredosantana <santana.wil@gmail.com>
2017-09-12 21:00:26 -0400

    Sample mongo database started

--- fswd/07_node.js/03_mongodriver/app.js
+++ fswd/07_node.js/03_mongodriver/app.js
@@ -9,8 +9,15 @@ MongoClient.connect(url, function(err, db){
     return console.dir(err);
   }
   console.log('Connected to mongodb');
-
+  /*
   InsertDocument(db, function(){
+    db.close();
+  });
+  InsertMultiDocs(db, function(){
+    db.close();
+  });
+  */
+  FindDocuments(db, function(){
     db.close();
   });
 });
@@ -30,5 +37,46 @@ const InsertDocument = function(db, callback) {
     console.log('Inserted Document');
     console.log(result);
     callback(result);
+  });
+}
+
+// Insert Multiple Documents
+
+const InsertMultiDocs = function(db, callback) {
+  const collection = db.collection('users');
+  collection.insertMany([
+    {
+      name: 'Wil Santana',
+      email: 'santana.wil@gmail.com'
+    },
+    {
+      name: 'Lucas Allison-Santana',
+      email: 'dog@chihuahua.com'
+    },
+    {
+      name: 'Fiona Edwards',
+      email: 'crazybetch@dominica.com'
+    }
+  ],
+  function(err, result){
+    if(err) {
+      return console.dir(err);
+    }
+    console.log('Inserted ' + result.ops.length + ' documents');
+    callback(result);
+  });
+}
+
+// Find documents
+
+const FindDocuments = function(db, callback) {
+  const collection = db.collection('users');
+  collection.find({}).toArray(function (err, docs){
+    if(err) {
+      return console.dir(err);
+    }
+    console.log('Found the following records ');
+    console.log(docs);
+    callback(docs);
   });
 }
