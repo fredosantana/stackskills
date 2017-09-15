2ef0961f6ab63fd2c3615ca515a3a1417a7cc6e0
fredosantana <santana.wil@gmail.com>
2017-09-15 06:30:09 -0400

    Passport sample app started

---
+++ fswd/07_node.js/04_passportapp/app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Express Session Middleware (https://github.com/expressjs/session)
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport Middleware (http://passportjs.org/)
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator Middleware (https://github.com/ctavan/express-validator)
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect-Flash Middleware
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Define routes
+app.use('/', routes);
+app.use('/users', users);
