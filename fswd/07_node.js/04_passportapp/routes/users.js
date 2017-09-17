12f074809e22273806b7f4009cd76d68419e7207
fredosantana <santana.wil@gmail.com>
2017-09-17 12:38:33 -0400

    EJS files render via respective js files

--- fswd/07_node.js/04_passportapp/routes/users.js
+++ fswd/07_node.js/04_passportapp/routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 // Login page - GET
 router.get('/login', function(req, res){
-  res.send('LOGIN');
+  res.render('login');
 });
 // Register page - POST
 router.get('/register', function(req, res){
-  res.send('REGISTER');
+  res.render('register');
 });
 
 module.exports = router;
