da8bd209357b5fac0e5ee8c54c45804c278d5ad7
fredosantana <santana.wil@gmail.com>
2017-09-17 20:46:50 -0400

    Logout functionality applied; mongodb section complete

--- fswd/07_node.js/04_passportapp/routes/index.js
+++ fswd/07_node.js/04_passportapp/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
+router.get('/', ensureAuthenthicated, function(req, res){
   res.render('index');
 });
 
+function ensureAuthenthicated(req, res, next){
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/users/login');
+}
 module.exports = router;
